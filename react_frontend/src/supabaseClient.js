d7433a15c10b9f5cbd03f45b5557d57d63113e35
Ryan <ryan@Ryans-MacBook-Pro.local>
2025-08-27 16:55:29 -0700

    supabase implementation

---
+++ react_frontend/src/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://ueyfoyrcjlrlmmzbonok.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVleWZveXJjamxybG1temJvbm9rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwNzY2MTEsImV4cCI6MjA3MTY1MjYxMX0.Qxhc7ptryIsT4gB83Mxo5tzRb0vZLs-RDOziON3lPcg';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
