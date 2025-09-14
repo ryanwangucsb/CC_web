aa801e7f49c9cb8c886febca3779ed7e8fd543da
Ryan <ryan@wedofarm.com>
2025-09-14 16:54:50 -0700

    Temporarily use IP address to avoid SSL certificate issues
    - Changed API_BASE_URL to https://18.144.58.149
    - This bypasses the SSL certificate issue with wedofarm.com
    - App should work while SSL certificate is being fixed

--- react_frontend/src/config.js
+++ react_frontend/src/config.js
@@ -1,5 +1,5 @@
-// Force the API URL to use wedofarm.com domain
-export const API_BASE_URL = 'https://wedofarm.com';
+// Use IP address with HTTPS to avoid SSL certificate issues
+export const API_BASE_URL = 'https://18.144.58.149';
 
 export const API_ENDPOINTS = {
   PRODUCTS: `${API_BASE_URL}/api/products/`,
