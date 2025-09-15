31d257ac007c85c0f81b5cc64bfacdfd8ccf04fb
Ryan <ryan@wedofarm.com>
2025-09-14 17:43:54 -0700

    Temporarily enable CORS for all origins
    - Set CORS_ALLOW_ALL_ORIGINS = True for debugging
    - This should fix the CORS issue immediately
    - Can be made more restrictive later once working

--- django_backend/django_backend/settings.py
+++ django_backend/django_backend/settings.py
@@ -114,7 +114,7 @@ CORS_ALLOWED_ORIGIN_REGEXES = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True  # Temporarily allow all origins for debugging
 
 # Supabase settings
 SUPABASE_URL = os.environ.get('SUPABASE_URL', 'https://ueyfoyrcjlrlmmzbonok.supabase.co')
